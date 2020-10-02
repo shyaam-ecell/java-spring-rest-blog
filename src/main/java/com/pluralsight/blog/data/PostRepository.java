0cb4d0c05ab4dead1fb750fb534703bbb885b3b1
Shyaam Shobanadevi <shyaam18101998@gmail.com>
2020-10-02 19:04:25 +0530

    "added other tasks"

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -2,9 +2,13 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    @RestResource(rel="contains-title",path="containsTitle")
+    public List<Post> findByTitleContaining(String title);
 }
